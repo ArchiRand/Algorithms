126fc81d60fc195f3014b5ed9d2d277e377cdf02
ArchiRand <archirand@gmail.com>
2017-05-10 13:28:50 +0300

    Update SelectionSort.java

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -27,30 +27,3 @@ public class SelectionSort {
         a[i] = a[j];
         a[j] = temp;
     }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-        for (int i = 0; i < a.length; i++) {
-            int min = a[i];
-            int index = i;
-            for(int j = i + 1; j < a.length; j++) {
-                if(a[j] < min) {
-                    min = a[j];
-                    index = j;
-                }
-            }
-            if(i != index)
-                swap(a, i, index);
-        }
- */
