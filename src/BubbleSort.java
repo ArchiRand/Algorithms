52002834ce00d7bdad778d979bb109136ae9b9af
ArchiRand <archirand@gmail.com>
2017-05-10 13:23:50 +0300

    Update BubbleSort.java

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -19,17 +19,3 @@ public class BubbleSort {
         a[j] = temp;
     }
 }
-
-
-
-
-
-
-
-
-/*
-for(int i = a.length - 1; i > 0; i--)
-            for(int j = 0; j < i; j++)
-                if(a[j] > a[j + 1])
-                    swap(a, j, j+1);
- */
